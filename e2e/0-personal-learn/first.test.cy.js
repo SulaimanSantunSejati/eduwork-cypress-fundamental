1a9dc33cd044de0478ed54b2bf63f652ababacf0
SulaimanSantunSejati <sulaimansantun.s@gmail.com>
2024-09-18 16:14:34 +0700

    first commit

---
+++ e2e/0-personal-learn/first.test.cy.js
@@ -0,0 +1,13 @@
+/// <reference types="cypress" />
+
+describe('My first test', () => {
+    it('Visit The Kitchen Sink', () => {
+        cy.visit('https://example.cypress.io')
+        cy.get('h1').contains('Kitchen Sink')
+        cy.contains('get').click()
+            cy.url().should('include', '/commands/querying')
+        
+    });    
+});
+
+
